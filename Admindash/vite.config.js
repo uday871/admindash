8ce2729fa9157684b873727656aa2ef34228b54d
uday871 <uday871@gmail.com>
2024-09-24 22:27:30 +0530

    not

--- Admindash/vite.config.js
+++ Admindash/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/admindash/',
+  base: '/admindash/', // Make sure this is correct
   plugins: [react()],
-})
+});
